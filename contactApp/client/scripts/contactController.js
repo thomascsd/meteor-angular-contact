20005592673509375fdcb3cbbef4681f3a60acd1
thomas chang <thomascsd808@gmail.com>
2015-11-18 09:00:42 +0000

    Update to Meteor 1.2.1

--- contactApp/client/scripts/contactController.js
+++ contactApp/client/scripts/contactController.js
@@ -2,7 +2,7 @@ var contactApp = angular.module('contactApp');
 
 contactApp.controller('contactController', ['$scope', '$meteor', function($scope, $meteor) {
 
-    $scope.contacts = $meteor.conllection(Contacts).subcribe();
+    $scope.contacts = $meteor.conllection(Contacts).subscribe();
 
 
     $scope.addContact = function(e, contact) {
