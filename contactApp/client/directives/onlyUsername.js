5a05d3c51d3eb63f127650ec4c6e6ad7341a3a2a
thomascsd <thomascsd808@gmail.com>
2016-03-01 19:54:48 +0800

    add onlyUsername

--- contactApp/client/directives/onlyUsername.js
+++ contactApp/client/directives/onlyUsername.js
@@ -5,8 +5,10 @@ let contactApp = angular.module('contactApp');
 contactApp.directive('onlyUsername', function() {
     return {
         restrict: 'A',
-        controller: function($element) {
+        controller: function(scope, element, attrs, ctrl) {
+            ctrl.$validators.onlyUsername = function(modeValue, viewValue) {
 
+            }
         }
     };
 })
