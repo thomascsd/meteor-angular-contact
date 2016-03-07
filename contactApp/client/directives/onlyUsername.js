d83027c663037c2cc245e05a080d93150d7bf901
thomascsd <thomascsd808@gmail.com>
2016-03-08 00:32:02 +0800

    add onlyUsername features

--- contactApp/client/directives/onlyUsername.js
+++ contactApp/client/directives/onlyUsername.js
@@ -5,8 +5,24 @@ let contactApp = angular.module('contactApp');
 contactApp.directive('onlyUsername', function() {
     return {
         restrict: 'A',
-        controller: function(scope, element, attrs, ctrl) {
+        require: 'ngModel',
+        controller: function($element) {
+            var ctrl = $element.controller('ngModel');
+            
             ctrl.$validators.onlyUsername = function(modeValue, viewValue) {
+                if (viewValue) {
+                    var user = Contacts.findOne({
+                        'name': viewValue
+                    }).fetch();
+
+                    if (user) {
+                        //Has same user
+                        return false;
+                    }
+                    return true;
+                } else {
+                    return true;
+                }
 
             }
         }
