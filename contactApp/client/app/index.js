a76435f4645c2cb5bf841b1041b463083563cfaf
thomascsd <thomascsd808@gmail.com>
2016-11-06 16:17:59 +0800

    add angular-material css

--- contactApp/client/app/index.js
+++ contactApp/client/app/index.js
@@ -3,8 +3,8 @@ import ngMessages from 'angular-messages';
 import ngMaterial from 'angular-material';
 import * as angularMeteor from 'angular-meteor';
 
-angular.module('contactApp', ['angular-meteor', 'angular-meteor.auth', 'ngMessages', 'ngMaterial'])
-    .config(['$mdThemingProvider', '$mdIconProvider', function($mdThemingProvider, $mdIconProvider) {
+angular.module('contactApp', ['ngMaterial', 'angular-meteor', 'angular-meteor.auth', 'ngMessages'])
+    .config(['$mdThemingProvider', '$mdIconProvider', function ($mdThemingProvider, $mdIconProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('brown')
             .accentPalette('deep-orange');
